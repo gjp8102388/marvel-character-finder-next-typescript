741ac56d8a652fec194cbd5932a238c499d1065a
Aaron-Guo <gjp77440169@gmail.com>
2022-03-06 15:18:21 +1100

    Change base url

--- common/apiUtils.ts
+++ common/apiUtils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://gateway.marvel.com/';
+const baseUrl = 'http://gateway.marvel.com';
 const apikey = 'f4056f0d40a4ceb06847f85f18b154c7';
 export const getCharacterList = (nameStartsWith: string) =>
     axios({
