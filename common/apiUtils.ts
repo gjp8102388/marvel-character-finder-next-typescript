537988c10a9083197acb9212eeddf47a30f5b97f
Aaron-Guo <gjp77440169@gmail.com>
2022-03-06 12:03:25 +1100

    Change env variables

--- common/apiUtils.ts
+++ common/apiUtils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseUrl = (process.env.NODE_ENV === 'development')?(process.env.NEXT_PUBLIC_API_URL):(process.env.API_URL);
-const apikey = (process.env.NODE_ENV === 'development')?(process.env.NEXT_PUBLIC_API_KEY):(process.env.API_KEY);
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const apikey = process.env.NEXT_PUBLIC_API_KEY;
 export const getCharacterList = (nameStartsWith: string) =>
     axios({
         method: 'get',
