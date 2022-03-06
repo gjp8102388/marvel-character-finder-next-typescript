64cf5598503ab2e8d7d7a74931a2a981fd94d6aa
Aaron-Guo <gjp77440169@gmail.com>
2022-03-06 15:51:35 +1100

    Remove console log and change to https

--- common/apiUtils.ts
+++ common/apiUtils.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 const md5 = require("md5");
-const baseUrl = 'http://gateway.marvel.com';
+const baseUrl = 'https://gateway.marvel.com';
 const apikey = 'f4056f0d40a4ceb06847f85f18b154c7';
 const privatekey = '8e289ce5f58ea672690f332eb3a369838c145a27';
 const ts = Date.now();
 const hash = md5(ts+privatekey+apikey);
-console.log(hash);
 export const getCharacterList = (nameStartsWith: string) =>
     axios({
         method: 'get',
