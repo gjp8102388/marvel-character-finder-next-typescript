d71ef4f46eebd6a115de3baa76477ca16fed6e7d
Aaron-Guo <gjp77440169@gmail.com>
2022-03-06 16:26:33 +1100

    Change env variables

--- common/apiUtils.ts
+++ common/apiUtils.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 const md5 = require("md5");
-const baseUrl = process.env.BASE_URL;
-const apikey = process.env.PUBLIC_KEY;
-const privatekey = process.env.PRIVATE_KEY;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const apikey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const privatekey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
 const ts = Date.now();
+// @ts-ignore
 const hash = md5(ts+privatekey+apikey);
 export const getCharacterList = (nameStartsWith: string) =>
     axios({
